88cce238c067098ab40f2ed46833ca3b71eb2dff
Arfi Renaldi <arfirenaldi@gmail.com>
2022-08-05 21:18:40 +0700

    initiate project

---
+++ src/views/MovieView.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      data: ''
+    }
+  },
+  mounted() {
+    console.log(this.$route.params.id)
+  },
+}
+</script>
+
+<template>
+  <main>
+    <h1>Movie Detail</h1>
+    <p>{{ this.$route.params.id }}</p>
+  </main>
+</template>
---
+++ src/views/MoviesView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Movies</h1>
+  </main>
+</template>
