66d4d66021003145b76afdd192222e3a5ea32db1
Arfi Renaldi <arfirenaldi@gmail.com>
2022-08-08 14:53:45 +0700

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <footer class="bg-[#15191E]">
+    <div class="container mx-auto">
+      <div class="flex h-[160px] items-center">
+        <p class="flex-1 text-sm text-[#929292] font-normal">© 2021 MoovieTime. All rights reserved.</p>
+        <img src="../assets/logo-grey.svg" alt="logo grey" class="flex-1 h-6" />
+        <p class="flex-1 text-sm text-[#929292] font-normal text-right">Made with Vue.js</p>
+      </div>
+    </div>
+  </footer>
+</template>
